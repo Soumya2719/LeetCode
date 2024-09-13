51e7329d8801b645fc8d730587576e44e21bbc57
Soumya2719 <150850854+Soumya2719@users.noreply.github.com>
2024-09-13 23:35:58 +0530

    Time: 7 ms (20.92%), Space: 42.8 MB (15.8%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,26 +3,30 @@ class Solution {
         int n1=word1.length();
         int n2=word2.length();
        String str="";
-       if(n1==n2){
-        for(int i=0;i<n1;i++){
-            str+=word1.charAt(i);
-            str+=word2.charAt(i);
+        if(n1==n2){
+            for(int i=0;i<n1;i++){
+                str+=word1.charAt(i);
+                str+=word2.charAt(i);
+            }
         }
-       }
-       else if(n1<n2){
-        for(int i=0;i<n1;i++){
-            str+=word1.charAt(i);
-             str+=word2.charAt(i);
-        }
-        str+=word2.substring(n1,n2);
-       }
-       else{
-         for(int i=0;i<n2;i++){
-           str+=word1.charAt(i);
-             str+=word2.charAt(i);
-        }
-        str+=word1.substring(n2,n1);
-       }
-       return str;
+        else if(n1<n2){
+                for(int i=0;i<n1;i++){
+                 str+=word1.charAt(i);
+                 str+=word2.charAt(i);
+                }
+              for(int i=n1;i<n2;i++){
+                str+=word2.charAt(i);
+              }
+            }
+            else{
+                for(int i=0;i<n2;i++){
+                 str+=word1.charAt(i);
+                 str+=word2.charAt(i);
+                }
+              for(int i=n2;i<n1;i++){
+                str+=word1.charAt(i);
+              }
+            }
+        return str;
     }
 }
