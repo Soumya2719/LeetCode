8244af245240b00c2c764034949c5f3bb8046b63
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-09-22 21:24:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        class Solution {
+    public int smallestEvenMultiple(int n) {
+        if((n & 1) == 0)
+            return n;
+        else
+            return 2*n;
+    }
+}
+    }
+}
