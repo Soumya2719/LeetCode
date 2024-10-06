9c6920ec397af469a974dabfa6d87099fcd345d6
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-10-06 21:34:59 +0530

    Time: 0 ms (100%), Space: 41 MB (95.89%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,6 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-            Queue <Character> q=new LinkedList<>();
+           /* Queue <Character> q=new LinkedList<>();
             q.add(address.charAt(0));
             for(int i=1;i<address.length();i++){
                 if(address.charAt(i)=='.'){
@@ -16,6 +16,7 @@ class Solution {
             while(q.size()!=0){
                 res=res+q.poll();
             }
-            return res;
+            return res;*/
+            return address.replace(".","[.]");
     }
 }
