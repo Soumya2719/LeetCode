4fb76a772a34cbb4725c698a01a836cdc0f15e81
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-10-07 01:43:39 +0530

    Time: 37 ms (10.97%), Space: 44.7 MB (7.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int fib(int n) {
-       if(n==0){
+     /*  if(n==0){
         return 0;
        } 
        if(n==1){
@@ -12,6 +12,18 @@ class Solution {
        for(int i=2;i<=n;i++){
         dp[i]+=dp[i-1]+dp[i-2];
        }
-       return dp[n];
+       return dp[n];*/
+       int dp[]=new int[n+1];
+       Arrays.fill(dp,-1);
+       if(n==0 || n==1){
+        return n;
+       }
+       if(dp[n]!=-1){
+        return dp[n];
+       }
+       int a=fib(n-1);
+       int b=fib(n-2);
+       dp[n]=a+b;
+       return a+b;
     }
 }
