936e54509f60779b0ddd50cda83a7010184c43b2
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-10-13 12:25:26 +0530

    Time: 1 ms (53.89%), Space: 41.9 MB (62.35%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.java
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -3,7 +3,7 @@ class Solution {
        int a=nums[0];
        int b=nums[1];
        int c=nums[2];
-       if(a+b<c || b+c<a || c+a<b){
+       if(a+b<=c || b+c<=a || c+a<=b){
         return "none";
        }
        if(a==b && b==c){
