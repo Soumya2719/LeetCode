fcb9e2d2e53316672d8d950403d2e85576ef15cd
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-11-01 19:13:52 +0530

    Time: 2 ms (96.98%), Space: 43.3 MB (49.11%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,36 +1,21 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {   
-        HashMap <Integer,Integer> hs1=new HashMap<>();
-        HashMap <Integer,Integer> hs2=new HashMap<>();
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            if(hs1.containsKey(nums1[i])){
-                int temp=hs1.get(nums1[i]);
-                hs1.put(nums1[i],temp+1);
-            }
-            else{
-                hs1.put(nums1[i],1);
-            }
+        HashSet <Integer> hs1=new HashSet<>();
+        HashSet <Integer> hs2=new HashSet<>();
+        for(int k:nums1){
+            hs1.add(k);
         }
-        for(int i=0;i<nums2.length;i++){
-            if(hs2.containsKey(nums2[i])){
-                int temp=hs2.get(nums2[i]);
-                hs2.put(nums2[i],temp+1);
-            }
-            else{
-                hs2.put(nums2[i],1);
-            }
+         for(int k:nums2){
+            if(hs1.contains(k))
+                 hs2.add(k);
         }
-        for(int k:hs1.keySet()){
-            if(hs1.containsKey(k) && hs2.containsKey(k)){
-               list.add(k);
-        }
-        }
-        int size=list.size();
-        int arr[]=new int[size];
-        for(int i=0;i<size;i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
+      int n=hs2.size();
+      int arr[]=new int[n];
+      int i=0;
+      for (int k:hs2){
+        arr[i]=k;
+        i++;
+      }
+    return arr;
       }
 }
