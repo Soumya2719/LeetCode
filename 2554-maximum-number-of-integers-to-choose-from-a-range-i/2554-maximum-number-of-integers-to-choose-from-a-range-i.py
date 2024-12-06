5ba44eb44c735bf4750ad48dc4c35988841b47d6
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2024-12-06 22:44:58 +0530

    Time: 59 ms (78.26%), Space: 13.6 MB (9.38%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxCount(self, banned, n, maxSum):
+       banned_set = set(banned)
+       total_sum = 0
+       count = 0
+       for i in range(1, n + 1):
+            if i in banned_set:
+                continue
+            total_sum += i
+            if total_sum > maxSum:
+                break
+            count += 1
+       return count
+        
+        
+
+        
+     
