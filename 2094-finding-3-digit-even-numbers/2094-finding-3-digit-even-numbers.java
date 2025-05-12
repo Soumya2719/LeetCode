32220a65601107c335565df0ad2d149fcd47b23a
Soumya Ranjan Mallick <150850854+Soumya2719@users.noreply.github.com>
2025-05-12 21:48:40 +0530

    Time: 31 ms (43.67%), Space: 44.6 MB (82.46%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -1,30 +1,40 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        
-      HashSet<Integer> list=new HashSet<>();
-
-      for(int i=0;i<digits.length;i++){
-        for(int j=0;j<digits.length;j++){
-            for(int k=0;k<digits.length;k++){
-                if(i!=j && i!=k && j!=k){
-                      int temp=Integer.parseInt(number(digits[i],digits[j],digits[k]));
-                      if(temp%2==0 && temp>99){
-                        list.add(temp);
-                      }
-                }
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<digits.length;i++){
+            if(hm.containsKey(digits[i])){
+                int temp=hm.get(digits[i]);
+                hm.put(digits[i],temp+1);
+            }
+            else{
+                hm.put(digits[i],1);
             }
         }
-      }
-      int arr[]=new int[list.size()];
-      int l=0;
-       for(int i:list){
-          arr[l++]=i;
+
+        for(int i=100;i<1000;i+=2){
+            int a=i/100;
+            int b=(i/10)%10;
+            int c=i%10;
+            if(hm.containsKey(a) && hm.get(a)>0){
+                  hm.put(a,hm.get(a)-1);
+                   if(hm.containsKey(b) && hm.get(b)>0){
+                       hm.put(b,hm.get(b)-1);
+                        if(hm.containsKey(c) && hm.get(c)>0){
+                            hm.put(c,hm.get(c)-1);
+                            list.add(i);
+                            hm.put(c,hm.get(c)+1);
+                        }
+                         hm.put(b,hm.get(b)+1);
+                   }
+                    hm.put(a,hm.get(a)+1);
+            }
+        }
+   int arr[]=new int[list.size()];
+       for(int i=0;i<arr.length;i++){
+           arr[i]=list.get(i);
        }
-      Arrays.sort(arr);
-      return arr;
-    }
-    public static String number(int a,int b,int c){
-        String temp=""+a+b+c;
-        return temp;
+       Arrays.sort(arr);
+       return arr;
     }
 }
